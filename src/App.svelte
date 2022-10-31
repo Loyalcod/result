5919e41dfbb45e5aff8fde3c6df218d5f01e7c24
akujuobi victor <victorakujuobichibuike@gmail.com>
2022-10-31 20:52:18 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+
+
+<main>
+	<div class="bg-primary m-1 p-1">small margin and padding</div>
+	<div class="bg-primary m-5 p-5">large margin and padding</div>
+	<div class="bg-primary my-5 px-3">margin y direction and padding in x direction</div>
+	<div class="bg-primary mt-3 mb-4 ps-5 pt-4 pb-3 pe-4"> margin and padding for each direction</div>
+
+	<div class="m-3 p-3 border">defult border</div>
+	<div class="m-2 m-3 border-top border-end">individual border</div>
+</main>
